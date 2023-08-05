bd0ca013a578585065240dde75f50f92a57f2447
smart <mail@>
2023-08-05 11:56:14 +0200

    added buttons

--- src/App.js
+++ src/App.js
@@ -10,35 +10,32 @@ const images = [
 ];
 
 export default function App() {
-  const [current, setCurrent] = useState(0);
+  const [image, setImage] = useState(0);
 
-  function nextSlide() {
-    setCurrent(current === images.length - 1 ? 0 : current + 1);
+  function next() {
+    setImage(image === images.length -1 ? 0: image + 1);
   }
-
-  function prevSlide() {
-    setCurrent(current === 0 ? images.length - 1 : current - 1);
+  function previous(){
+    setImage(image === images.length - 1 ? 0: image -1);
   }
 
   return (
-      <div>
-        <h2>Project 1: Carousel</h2>
-        <div className="slider">
-          <div className="left-arrow" onClick={prevSlide}>
-            ⬅
-          </div>
-          <div className="right-arrow" onClick={nextSlide}>
-            ⮕
-          </div>
-          {images.map(
-              (image, index) =>
-                  current === index && (
-                      <div key={image} className="slide">
-                        <img src={image} alt="images" />
-                      </div>
-                  )
-          )}
-        </div>
+    <div>
+      <h2>Project Carousel</h2>
+      <div className="slider">
+        <button className="right-arrow" onClick={next}> ⮕ </button>
+        <button className="left-arrow" onClick={previous}> ⬅ </button>
+        {images.map ((pic, index) =>
+          image === index && (
+            <div key={image} className="slide">
+              <img src={pic} alt="image"/>
+            </div>
+            )
+        )}
+
       </div>
+    </div>
   );
+
+
 }
